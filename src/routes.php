dc181ba6d7d82b4dc5f92e504522e9262bfaa8af
Steve Ronuken <steve@fuzzysteve.co.uk>
2016-07-25 15:54:57 +0200

    New updates

--- src/routes.php
+++ src/routes.php
@@ -1,16 +1,219 @@
 <?php
 // Routes
 
+$app->get('/appraisal/',function ($request, $response, $args) {
+    return $this->renderer->render($response, 'appraisal.phtml', $args);    
+});
+
+$app->post('/appraisal/',function ($request, $response, $args) {
+    
+    $region=$_POST['region'];
+    if (!ctype_digit($region)) {
+        exit;
+    }
+
+    $db = new PDO("pgsql:host=localhost;dbname=marketdata;user=marketdata;password=marketdatapass");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $sql=<<<EOS
+select lower("typeName") "typeName","typeID" from evesde."invTypes" where published=true and "marketGroupID" is not null
+EOS;
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $typeidlookup=array();
+    while ($row = $stmt->fetchObject()) {
+            $typeidlookup[$row->typeName]=$row->typeID;
+    }
+    $inventory=array();
+
+    if (array_key_exists('pasteblock',$_POST)) {
+        $entries=explode("\n", $_POST['pasteblock']);
+    } else {
+        exit;
+    }
+
+    foreach ($entries as $entry) {
+        $entry=strtolower(preg_replace('/(\d),(\d)/','$1$2',$entry));
+        if (preg_match("/^(30 Day Pilot.*)\t(\d+)\t(.*)$/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$matches[2];
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$matches[2];
+                }
+            }
+        } elseif (preg_match("/^(\d+) (.*)$/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[2]])) {
+                if (isset($inventory[$typeidlookup[$matches[2]]])) {
+                    $inventory[$typeidlookup[$matches[2]]]+=$matches[1];
+                } else {
+                    $inventory[$typeidlookup[$matches[2]]]=$matches[1];
+                }
+            }
+        } elseif (preg_match("/^(.*)\t([\d.,]+)\t/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=str_replace(',', '', str_replace(',', '', $matches[2]));
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^(.*):\s+([\d.,]+)/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=str_replace(',', '', str_replace(',', '', $matches[2]));
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^(.*)\t([\d.,]+)/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=str_replace(',', '', str_replace(',', '', $matches[2]));
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^\[(.*),.*]/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=1;
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^(.*), Qty: (\d+)/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=$matches[2];
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^.*\t(.*)\t.*/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=1;
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        } elseif (preg_match("/^(.*)/", trim($entry), $matches)) {
+            if (isset($typeidlookup[$matches[1]])) {
+                $quantity=1;
+                if (isset($inventory[$typeidlookup[$matches[1]]])) {
+                    $inventory[$typeidlookup[$matches[1]]]+=$quantity;
+                } else {
+                    $inventory[$typeidlookup[$matches[1]]]=$quantity;
+                }
+            }
+        }
+    }
+
+    if (count($inventory)) {
+        $sql="insert into appraisal (identifier,list) values (:id,:list)"; 
+        $stmt = $db->prepare($sql);
+        $identifier=uniqid();
+        $stmt->execute(array(":id"=>$identifier,":list"=>json_encode($inventory)));
+
+        return $response->withRedirect('/appraisal/'.$region.'/'.$identifier);
+    }
+    return $this->renderer->render($response, 'appraisal.phtml', $args);
+});
+
+
+$app->get('/appraisal/{region:[0-9]+}/{identifier}',function ($request, $response, $args) {
+
+    $db = new PDO("pgsql:host=localhost;dbname=marketdata;user=marketdata;password=marketdatapass");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $sql=<<<EOS
+select "typeName","typeID",volume from evesde."invTypes" where published=true and "marketGroupID" is not null
+EOS;
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $typenamelookup=array();
+    while ($row = $stmt->fetchObject()) {
+            $typenamelookup[$row->typeID]=array("typeName"=>$row->typeName,"volume"=>$row->volume);
+    }
+    $sql='select "regionName","regionID" from evesde."mapRegions" where "regionID"=:region';
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array(":region"=>$args['region']));
+    $row = $stmt->fetchObject();
+    $regionName=$row->regionName;
+    $regionid=$row->regionID;
+
+    $args['region']=$regionName;
+    $args['regionid']=$regionid;
+
+    $sql='select list from appraisal where identifier=:id';
+    $stmt = $db->prepare($sql);
+    $stmt->execute(array(":id"=>$args['identifier']));
+    $row = $stmt->fetchObject();
+    $list=$row->list;
+
+    $items=json_decode($list);
+    
+    $redis = new Predis\Client();
+
+    $appraisal=array();
+    $total=array();
+    $total['pbuy']=0;
+    $total['mbuy']=0;
+    $total['psell']=0;
+    $total['msell']=0;
+    $total['volume']=0;
+    foreach ($items as $item=>$quantity) {
+        $buydetails=explode("|",$redis->get($regionid.'|'.$item."|true"));
+        $selldetails=explode("|",$redis->get($regionid.'|'.$item."|false"));
+        $line=array();
+        $line['typeid']=$item;
+        $line['typename']=$typenamelookup[$item]['typeName'];
+        $line['quantity']=$quantity;
+        $line['pbuy']=$quantity*$buydetails[7];
+        $line['psell']=$quantity*$selldetails[7];
+        $line['mbuy']=$quantity*$buydetails[1];
+        $line['msell']=$quantity*$selldetails[2];
+        $line['volume']=$quantity*$typenamelookup[$item]['volume'];
+        $total['pbuy']+=$line['pbuy'];
+        $total['mbuy']+=$line['mbuy'];
+        $total['psell']+=$line['psell'];
+        $total['msell']+=$line['msell'];
+        $total['volume']+=$line['volume'];
+        $appraisal[]=$line;
+    }
+
+    $args['appraisal']=$appraisal;
+    $args['total']=$total;
+    return $this->renderer->render($response, 'displayappraisal.phtml', $args);    
+
+});
+
+
 $app->get('/aggregates/',function ($request, $response, $args) {
     $redis = new Predis\Client();
     $aggregate=array();
     $allGetVars = $request->getQueryParams();
-    $region=$allGetVars['region'];
+    if (isset($allGetVars['region'])){
+        $region=$allGetVars['region'];
+    } elseif (isset($allGetVars['station'])){
+         $region=$allGetVars['station'];
+    }
     $types=$allGetVars['types'];
     $ordertype=array("true"=>"buy","false"=>"sell");
     foreach (explode(",",$types) as $type) {
+        if ($type == '') {
+            continue;
+        }
         foreach (array("true","false") as $buy) {
             $details=explode("|",$redis->get($region.'|'.$type."|".$buy));
+            if ($details[0]==""){
+                $details[0]=null;
+            }
             $aggregate[$type][$ordertype[$buy]]=array(
                 "weightedAverage"=>$details[0],
                 "max"=>$details[1],
@@ -23,6 +226,7 @@ $app->get('/aggregates/',function ($request, $response, $args) {
             );
         }
     }
+    $response=$response->withHeader('Access-Control-Allow-Origin', '*');
     $resWithExpires = $this->cache->withExpires($response->withJson($aggregate), time() + 300);
     return $resWithExpires;
 });
@@ -35,11 +239,12 @@ $app->get('/', function ($request, $response, $args) {
     foreach (array(34,35,36,37,38,39,40,11399,29668,40520) as $type) {
         $aggregate[$type]=array();
         foreach (array("true","false") as $buy) {
-            $aggregate[$type][$buy]=explode("|",$redis->get('10000002|'.$type."|".$buy));
+            $aggregate[$type][$buy]=explode("|",$redis->get('60003760|'.$type."|".$buy));
         }
     }
     $args['fpbuy']=json_decode($redis->get('fp-buy'));
     $args['fpsell']=json_decode($redis->get('fp-sell'));
+    $args['fplastupdate']=$redis->get('fp-lastupdate');
     $args['types']=array("Tritanium","Pyrite","Mexallon","Isogen","Nocxium","Zydrine","Megacyte","Morphite","PLEX","Skill Injector");
     $args['maggs']=$aggregate;
     return $this->renderer->render($response, 'index.phtml', $args);
